79e23ec8582cf716732cba74826e7bc70258158a
Landon <laba2346@colorado.edu>
2017-04-27 19:17:44 -0600

    meme

--- geometry_test.c
+++ geometry_test.c
@@ -90,6 +90,22 @@ START_TEST(test_2d_dist)
 
 }
 END_TEST
+START_TEST(test_2d_area){
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
+	a.x = 0;
+	a.y = 0;
+	b.x = 4;
+	b.y = 0;
+	c.x = 0;
+	c.y = 4;
+
+	double result = coord_2d_area_triangle(&a, &b, &c);
+	double expected = 8.00;
+	ck_assert(result == expected);
+}
+END_TEST
 
 /* coord_2d_midpoint Test */
 START_TEST(test_2d_midpoint)
@@ -161,16 +177,21 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
+	
+    	TCase* tc_2d_area = tcase_create("coord_2d_test");
+    	tcase_add_test(tc_2d_area, test_2d_area);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
 
 }
+
 
 /* main: run test suites and set exit status */
 int main(void){
